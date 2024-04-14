33f423618db0aeead1640e053ee8ccf563d495ad
Harrietstudent <166915351+Harrietstudent@users.noreply.github.com>
2024-04-14 23:41:10 +1000

    Create secret.h

---
+++ secret.h
@@ -0,0 +1,8 @@
+// I have removed all my details and replaced them 
+// with the string xxxxx
+
+#define SECRET_SSID "xxxxx"
+#define SECRET_PASS "xxxxx"
+
+#define SECRET_CH_ID xxxxx
+#define SECRET_WRITE_APIKEY "xxxxx"
